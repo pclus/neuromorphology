ee39c9a088fe5b570f5f96474d843c56db1e97c9
pclus <pau.clusella@upf.edu>
2023-05-05 07:06:42 +0200

    threading

--- thread.sh
+++ thread.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for i in {WT,Ts65Dn,TgDyrk1A};
+#for i in {WT,Ts65Dn,TgDyrk1A};
+#do
+#	./netdyn outputs/network${i}.dat sim${i} 9.0 1.0
+#done
+
+for i in results/net*dat;
 do
-	./netdyn outputs/network${i}.dat sim${i} 9.0 1.0
+	for j in {1..10};
+	do
+		nn=${i/results\//}
+		nn=${nn/.dat/}
+		./netdyn ${i} sim${j}_${nn} 9.0 1.0
+	done
 done
+
