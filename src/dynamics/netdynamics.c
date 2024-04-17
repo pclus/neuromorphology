8a095c76e331e6742c82a9961d964c65aa2aa32c
pclusella <pau.clusella@upf.edu>
2024-04-17 17:29:32 +0200

    isi in the src code

--- src/dynamics/netdynamics.c
+++ src/dynamics/netdynamics.c
@@ -6,13 +6,12 @@ int main(int argc, char **argv ){
 
 	FILE *fout,*fout_lfp,*fout_rp;
 	char *namm=malloc(sizeof(char)*200);
-	sprintf(namm,"outputs/spykes_%s.dat",argv[2]);
-	fout=fopen(namm,"w");
-	sprintf(namm,"outputs/lfp_%s.dat",argv[2]);
-	fout_lfp=fopen(namm,"w");
+	// sprintf(namm,"outputs/spykes_%s.dat",argv[2]);
+	// fout=fopen(namm,"w");
+	// sprintf(namm,"outputs/lfp_%s.dat",argv[2]);
+	// fout_lfp=fopen(namm,"w");
 	sprintf(namm,"outputs/rp_%s.dat",argv[2]);
 	fout_rp=fopen(namm,"w");
-	free(namm);
 
 	long unsigned count=0;
 	while( t<.5e4 ){
@@ -23,12 +22,12 @@ int main(int argc, char **argv ){
 		
 		if(t>500)
 		{
-			if(count%200==0)
-			{
-				fprintf(fout_lfp,"%lf %lf\n",t,activity/(1.0*N));
-				fprintf(fout,"%lf %lf\n",t,acti*1.0);
-			}
-			acti=0;
+			//if(count%200==0)
+			//{
+			//	fprintf(fout_lfp,"%lf %lf\n",t,activity/(1.0*N));
+			//	fprintf(fout,"%lf %lf\n",t,acti*1.0);
+			//}
+			//acti=0;
                 	for(i=0;i<N;i++)
 			{
                       		if(new_spyke[i]==1)
@@ -37,17 +36,43 @@ int main(int argc, char **argv ){
 		}
 		if(count%1000==0)
 		{
-			fflush(fout);
-			fflush(fout_lfp);
+			//fflush(fout);
+			//fflush(fout_lfp);
 			fflush(fout_rp);
 		}
 
 
 	}
+
+
+
 	fprintf(stderr,"\n");
-	fclose(fout);
-	fclose(fout_lfp);
+	//fclose(fout);
+	//fclose(fout_lfp);
 	fclose(fout_rp);
 	system_finish();
+
+	FILE *fin = fopen(namm,"r");
+	sprintf(namm,"outputs/isi_%s.dat",argv[2]);
+	fout = fopen(namm,"w");
+	double ts,*spike;
+	int k;
+
+	spike = (double *) malloc(sizeof(double)*N);
+	for(int j=0;j<N;j++) spike[j]=-1;
+
+	while(fscanf(fin,"%lf %d %d\n",&ts,&i,&k)!=EOF)
+	{
+		if(spike[i]>0)
+			fprintf(fout,"%d %lf %d\n",i,ts-spike[i],k);
+		spike[i]=ts;
+	}
+	free(spike);
+	free(namm);
+	fclose(fin);
+	fclose(fout);
+
+
+
 	return 1;
 }
