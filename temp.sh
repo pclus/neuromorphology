a94960a45adad2f1bfec864c5a59e8c666489eb3
pclusella <pau.clusella@upf.edu>
2024-04-25 21:44:01 +0200

    Update

--- temp.sh
+++ temp.sh
@@ -1,4 +1,27 @@
-./netdyn nets/netWT.dat simWT 9.0 1.0
-./netdyn nets/netTgDyrk1A.dat simTgDyrk1A 9.0 1.0
-./netdyn nets/netTs65Dn.dat simTs65Dn 9.0 1.0
+./netdyn networks/net_WT1.dat simWT_lam6_ixi 6.0 0.3
+./netdyn networks/net_TgDyrk1A1.dat simTgDyrk1A_lam6_ixi 6.0 0.3
+./netdyn networks/net_Ts65Dn1.dat simTs65Dn_lam6_ixi 6.0 0.3
+
+./netdyn networks/net_WT1.dat simWT_lam6 6.0 1.0
+./netdyn networks/net_TgDyrk1A1.dat simTgDyrk1A_lam6 6.0 1.0
+./netdyn networks/net_Ts65Dn1.dat simTs65Dn_lam6 6.0 1.0
+
+# topology:
+# alpha 0.1 and 2
+# beta 0.5 and 2
+
+./nettopology 16 78.15 40 500 6 0.1 
+cp outputs/network.dat networks/net_alpha0.1_beta0.5
+./nettopology 16 312.6 40 500 6 0.1
+cp outputs/network.dat networks/net_alpha0.1_beta2
+./nettopology 16 78.15 40 500 6 2
+cp outputs/network.dat networks/net_alpha2_beta0.5
+./nettopology 16 312.6 40 500 6 2
+cp outputs/network.dat networks/net_alpha2_beta2
+
+for i in networks/net_alpha*;
+do
+	name="$(basename -- $i)"
+	./netdyn $i sim_${name} 9.0 1.0
+done
 
